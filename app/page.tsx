c7d6ca8d905969349ec87e21f4820e78dc065da9
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-02-25 16:59:20 +0500

    15:00 also button added from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Lore Lag Gaye</h2>
+      <Button>Chal Bhag Bhosri ke</Button>
     </div>
   );
 }
